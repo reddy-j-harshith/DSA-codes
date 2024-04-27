3b59bb24985d7e0856d840bb0c236f47ea05145d
reddy-j-harshith <143481508+reddy-j-harshith@users.noreply.github.com>
2024-04-27 21:33:30 +0530

    Update 8th.c

--- Lab Assignments/3rd week/8th.c
+++ Lab Assignments/3rd week/8th.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Function to count the number of set bits (1s) in an integer
 int countSetBits(unsigned long long num) {
     int count = 0;
     while (num) {
@@ -21,19 +20,14 @@ int main() {
 
     int maxSatisfiedClients = 0;
     for (int i = 0; i <= n - k; i++) {
-        // Initialize a bitmask to keep track of satisfied clients
         unsigned long long satisfiedClients = 0;
 
-        // Iterate over the current segment of deals
         for (int j = i; j < i + k; j++) {
-            // Update the bitmask by OR-ing the bits of each deal
             satisfiedClients |= deals[j];
         }
 
-        // Count the number of set bits (satisfied clients) in the bitmask
         int count = countSetBits(satisfiedClients);
 
-        // Update the maximum count of satisfied clients
         if (count > maxSatisfiedClients) {
             maxSatisfiedClients = count;
         }
